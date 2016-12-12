dda6ec31108088a7b3ee0cce0602f3c639d57be1
yellowjim <yellowjim@foxmail.com>
2016-12-13 00:46:00 +0800

    地名普查照片命名工具

--- Guid-Photo.py
+++ Guid-Photo.py
@@ -79,24 +79,64 @@ def phototype(rootdir):
     elif re.compile('[0-9a-zA-Z]{16}').search(rootdir) and re.compile('[0-9a-zA-Z]{32}').search(rootdir):
         return ('landmark',re.compile('[0-9a-zA-Z]{32}').search(rootdir).group())
 
-def main():
-    pic_name=''
-    root = Tk()
-    root.geometry('500x360+720+320')
-    root.title('地名普查外业照片批量更名工具 Ver_1.0  YellowJim制作')
-    dirname = tkFileDialog.askdirectory(parent=root, initialdir="/", title='选择照片所在文件夹')
-    for rootdir,dirs,files in os.walk(dirname):
+def guid_mode():
+    global root
+    global pic_path
+    global db_path
+    pic_path = tkFileDialog.askdirectory(parent=root, initialdir="/", title='选择【 照 片 （*.JPG） 】所在文件夹')
+    while pic_path=='':
+        return 0
+    print pic_path
+    db_path=tkFileDialog.askdirectory(parent=root, initialdir="/", title='选择【Sqlite数据库(*.db)】文件所在文件夹')
+    while db_path=='':
+        return 0
+    for rootdir,dirs,files in os.walk(db_path):
         for files_name in files:
             make_db_data(os.path.join(rootdir,files_name))
-    for rootdir,dirs,files in os.walk(dirname):
+    for rootdir,dirs,files in os.walk(pic_path):
         for files_name in files:
             change_picname(os.path.join(rootdir,files_name))
+
+
+def featureid_mode():
+    global root
+    global pic_path
+    global db_path
+    pic_path = tkFileDialog.askdirectory(parent=root, initialdir="/", title='选择【 照 片 （*.JPG） 】所在文件夹')
+    while pic_path=='':
+        return 0
+    print pic_path
+    db_path=tkFileDialog.askdirectory(parent=root, initialdir="/", title='选择【Sqlite数据库(*.db)】文件所在文件夹')
+    while db_path=='':
+        return 0
+    for rootdir,dirs,files in os.walk(db_path):
+        for files_name in files:
+            make_db_data(os.path.join(rootdir,files_name))
+    for rootdir,dirs,files in os.walk(pic_path):
+        for files_name in files:
+            change_picname(os.path.join(rootdir,files_name))
+
+def main():
+    global root
+    global pic_path
+    global db_path
+    pic_name=''
+    root = Tk()
+    root.geometry('480x200+720+320')
+    root.title('地名普查外业照片批量更名工具 Ver_1.0  YellowJim制作')
+    Button(root,text = u'GUID 模式\n\n(此模式适合GUID没有发生变化时)',command=guid_mode,width = 70,height = 4,font='微软雅黑').pack()
+    Button(root, text=u'FeatureID 模式\n\n(此模式适合GUID发生变化，改用FeatureID匹配)', command=featureid_mode, width=70, height=4,font='微软雅黑').pack()
+    root.mainloop()
 
 if __name__ == '__main__':
     global guid
     global place_start
     global landmark_start
+    global pic_path
+    global db_path
     place_start=1
     landmark_start=11
     guid=''
+    pic_path=''
+    db_path=''
     main()
