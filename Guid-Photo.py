a7b199d42d52777d3d08af7abfa7c9a27dc324a0
yellowjim <yellowjim@foxmail.com>
2016-12-14 14:10:31 +0800

    照片更名

--- Guid-Photo.py
+++ Guid-Photo.py
@@ -7,25 +7,48 @@ import re
 import sqlite3
 db={}
 
-def make_db_data(file_name):
-    if re.match('\d{6,}.db$',os.path.basename(file_name)):
-        db_conn = sqlite3.connect(file_name)
-        db_cursor = db_conn.cursor()
-        temp=db_cursor.execute("select f_id,f_name from tb_landmark_info").fetchall()
-        db_conn.close()
-        for x in temp:
-            if x[1]=='':
-                continue
-            db[x[0]] = x[1]
-    elif re.match(r'F\d{2}[a-zA-Z]{1}\d{6}.db$',os.path.basename(file_name)):
-        db_conn = sqlite3.connect(file_name)
-        db_cursor = db_conn.cursor()
-        temp=db_cursor.execute("select f_id,f_name from tb_place_info").fetchall()
-        db_conn.close()
-        for x in temp:
-            if x[1]=='':
-                continue
-            db[x[0]] = x[1]
+def make_db_data(file_name,mode):
+    if mode=='guid':
+        if re.match('\d{6,}.db$',os.path.basename(file_name)):
+            db_conn = sqlite3.connect(file_name)
+            db_cursor = db_conn.cursor()
+            temp=db_cursor.execute("select f_id,f_name from tb_landmark_info").fetchall()
+            db_conn.close()
+            for x in temp:
+                if x[1]=='':
+                    continue
+                db[x[0]] = x[1]
+        elif re.match(r'F\d{2}[a-zA-Z]{1}\d{6}.db$',os.path.basename(file_name)):
+            db_conn = sqlite3.connect(file_name)
+            db_cursor = db_conn.cursor()
+            temp=db_cursor.execute("select f_id,f_name from tb_place_info").fetchall()
+            db_conn.close()
+            for x in temp:
+                if x[1]=='':
+                    continue
+                db[x[0]] = x[1]
+
+    elif mode=='fid':
+        if re.match('\d{6,}.db$',os.path.basename(file_name)):
+            db_conn = sqlite3.connect(file_name)
+            db_cursor = db_conn.cursor()
+            temp=db_cursor.execute("select f_id,f_idcode,f_name from tb_landmark_info").fetchall()
+            db_conn.close()
+            for x in temp:
+                if x[0]=='' or x[1]=='':
+                    continue
+                db[x[0]] = x[1]
+                db[x[1]] = x[2]
+        elif re.match(r'F\d{2}[a-zA-Z]{1}\d{6}.db$',os.path.basename(file_name)):
+            db_conn = sqlite3.connect(file_name)
+            db_cursor = db_conn.cursor()
+            temp=db_cursor.execute("select f_id,f_idcode,f_name from tb_place_info").fetchall()
+            db_conn.close()
+            for x in temp:
+                if x[0] == '' or x[1] == '':
+                    continue
+                db[x[0]] = x[1]
+                db[x[1]] = x[2]
 
 
 def make_landmark_db_data(file_name):
@@ -39,54 +62,125 @@ def make_landmark_db_data(file_name):
                 continue
             db[x[0]] = x[1]
 
-def change_picname(old_name):
+
+def change_picname(old_name,mode):
     global guid
     global place_start
     global landmark_start
-    if os.path.splitext(old_name)[1].lower()=='.jpg' and re.compile('[0-9a-zA-Z]{32}').search(old_name):
-        c_guid=re.compile('[0-9a-zA-Z]{32}').search(old_name).group()
-        if c_guid==guid and place_start<10:
-            try:
-                pic_name=db[c_guid]+'0'+str(place_start)+'.jpg'
-                new_name=os.path.join(os.path.split(old_name)[0],pic_name)
-                print old_name.split('\\')[-1]+u'  更名为  '+pic_name
-                os.rename(old_name,new_name)
-                place_start += 1
-            except:
-                pass
-        elif c_guid != guid:
-            try:
-                place_start = 1
-                pic_name=db[c_guid]+'0'+str(place_start)+'.jpg'
-                new_name=os.path.join(os.path.split(old_name)[0],pic_name)
-                print old_name.split('\\')[-1]+u'  更名为  '+pic_name
-                os.rename(old_name, new_name)
-                place_start+= 1
-                guid=c_guid
-            except:
-                pass
-    elif os.path.splitext(old_name)[1].lower()=='.jpg' and re.compile('[0-9a-zA-Z]{16}').search(old_name):
-        c_guid=re.compile('[0-9a-zA-Z]{16}').search(old_name).group()
-        if c_guid==guid and landmark_start<20:
-            try:
-                pic_name=db[c_guid]+str(landmark_start)+'.jpg'
-                new_name=os.path.join(os.path.split(old_name)[0],pic_name)
-                print old_name.split('\\')[-1]+u'  更名为  '+pic_name
-                os.rename(old_name, new_name)
-                landmark_start+=1
-            except:
-                pass
-        elif c_guid!=guid:
-            try:
-                landmark_start = 11
-                pic_name=db[c_guid]+str(landmark_start)+'.jpg'
-                new_name=os.path.join(os.path.split(old_name)[0],pic_name)
-                print old_name.split('\\')[-1]+u'  更名为  '+pic_name
-                os.rename(old_name, new_name)
-                landmark_start += 1
-                guid=c_guid
-            except:
-                pass
+    if mode=='guid':
+        if os.path.splitext(old_name)[1].lower()=='.jpg' and re.compile('[0-9a-zA-Z]{32}').search(old_name):
+            c_guid=re.compile('[0-9a-zA-Z]{32}').search(old_name).group()
+            if c_guid==guid and place_start<10:
+                try:
+                    pic_name=db[c_guid]+'0'+str(place_start)+'.jpg'
+                    new_name=os.path.join(os.path.split(old_name)[0],pic_name)
+                    print old_name.split('\\')[-1]+u'  更名为  '+pic_name
+                    try:
+                        os.rename(old_name,new_name)
+                    except:
+                        pass
+                    place_start += 1
+                except:
+                    pass
+            elif c_guid != guid:
+                try:
+                    place_start = 1
+                    pic_name=db[c_guid]+'0'+str(place_start)+'.jpg'
+                    new_name=os.path.join(os.path.split(old_name)[0],pic_name)
+                    print old_name.split('\\')[-1]+u'  更名为  '+pic_name
+                    try:
+                        os.rename(old_name,new_name)
+                    except:
+                        pass
+                    place_start+= 1
+                    guid=c_guid
+                except:
+                    pass
+        elif os.path.splitext(old_name)[1].lower()=='.jpg' and re.compile('[0-9a-zA-Z]{16}').search(old_name):
+            c_guid=re.compile('[0-9a-zA-Z]{16}').search(old_name).group()
+            if c_guid==guid and landmark_start<20:
+                try:
+                    pic_name=db[c_guid]+str(landmark_start)+'.jpg'
+                    new_name=os.path.join(os.path.split(old_name)[0],pic_name)
+                    print old_name.split('\\')[-1]+u'  更名为  '+pic_name
+                    try:
+                        os.rename(old_name,new_name)
+                    except:
+                        pass
+                    landmark_start+=1
+                except:
+                    pass
+            elif c_guid!=guid:
+                try:
+                    landmark_start = 11
+                    pic_name=db[c_guid]+str(landmark_start)+'.jpg'
+                    new_name=os.path.join(os.path.split(old_name)[0],pic_name)
+                    print old_name.split('\\')[-1]+u'  更名为  '+pic_name
+                    try:
+                        os.rename(old_name,new_name)
+                    except:
+                        pass
+                    landmark_start += 1
+                    guid=c_guid
+                except:
+                    pass
+    elif mode=='fid':
+        if os.path.splitext(old_name)[1].lower()=='.jpg' and re.compile('[0-9a-zA-Z]{32}').search(old_name):
+            c_guid=re.compile('[0-9a-zA-Z]{32}').search(old_name).group()
+            if c_guid==guid and place_start<10:
+                try:
+                    pic_name=db[db[c_guid]]+'0'+str(place_start)+'.jpg'
+                    new_name=os.path.join(os.path.split(old_name)[0],pic_name)
+                    print old_name.split('\\')[-1]+u'  更名为  '+pic_name
+                    try:
+                        os.rename(old_name,new_name)
+                    except:
+                        pass
+                    place_start += 1
+                except:
+                    pass
+            elif c_guid != guid:
+                try:
+                    place_start = 1
+                    pic_name=db[db[c_guid]]+'0'+str(place_start)+'.jpg'
+                    new_name=os.path.join(os.path.split(old_name)[0],pic_name)
+                    print old_name.split('\\')[-1]+u'  更名为  '+pic_name
+                    try:
+                        os.rename(old_name,new_name)
+                    except:
+                        pass
+                    place_start+= 1
+                    guid=c_guid
+                except:
+                    pass
+        elif os.path.splitext(old_name)[1].lower()=='.jpg' and re.compile('[0-9a-zA-Z]{16}').search(old_name):
+            c_guid=re.compile('[0-9a-zA-Z]{16}').search(old_name).group()
+            if c_guid==guid and landmark_start<20:
+                try:
+                    pic_name=db[db[c_guid]]+str(landmark_start)+'.jpg'
+                    new_name=os.path.join(os.path.split(old_name)[0],pic_name)
+                    print old_name.split('\\')[-1]+u'  更名为  '+pic_name
+                    try:
+                        os.rename(old_name,new_name)
+                    except:
+                        pass
+                    landmark_start+=1
+                except:
+                    pass
+            elif c_guid!=guid:
+                try:
+                    landmark_start = 11
+                    pic_name=db[db[c_guid]]+str(landmark_start)+'.jpg'
+                    new_name=os.path.join(os.path.split(old_name)[0],pic_name)
+                    print old_name.split('\\')[-1]+u'  更名为  '+pic_name
+                    try:
+                        os.rename(old_name,new_name)
+                    except:
+                        pass
+                    landmark_start += 1
+                    guid=c_guid
+                except:
+                    pass
 
 def guid_mode():
     global root
@@ -101,20 +195,20 @@ def guid_mode():
     if db_path==pic_path:
         for rootdir,dirs,files in os.walk(db_path):
             for files_name in files:
-                make_db_data(os.path.join(rootdir,files_name))
+                make_db_data(os.path.join(rootdir,files_name),'guid')
         for rootdir,dirs,files in os.walk(pic_path):
             for files_name in files:
-                change_picname(os.path.join(rootdir,files_name))
+                change_picname(os.path.join(rootdir,files_name),'guid')
     else:
         for rootdir,dirs,files in os.walk(db_path):
             for files_name in files:
-                make_db_data(os.path.join(rootdir,files_name))
+                make_db_data(os.path.join(rootdir,files_name),'guid')
         for rootdir,dirs,files in os.walk(pic_path):
             for files_name in files:
                 make_landmark_db_data(os.path.join(rootdir,files_name))
         for rootdir,dirs,files in os.walk(pic_path):
             for files_name in files:
-                change_picname(os.path.join(rootdir,files_name))
+                change_picname(os.path.join(rootdir,files_name),'guid')
     tkMessageBox.showinfo(title='', message='照片更名完毕！')
 
 
@@ -125,16 +219,20 @@ def featureid_mode():
     pic_path = tkFileDialog.askdirectory(parent=root, initialdir="/", title='选择【 照 片 （*.JPG） 】所在文件夹')
     while pic_path=='':
         return 0
-    print pic_path
     db_path=tkFileDialog.askdirectory(parent=root, initialdir="/", title='选择【Sqlite数据库(*.db)】文件所在文件夹')
     while db_path=='':
         return 0
-    for rootdir,dirs,files in os.walk(db_path):
-        for files_name in files:
-            make_db_data(os.path.join(rootdir,files_name))
     for rootdir,dirs,files in os.walk(pic_path):
         for files_name in files:
-            change_picname(os.path.join(rootdir,files_name))
+            make_db_data(os.path.join(rootdir,files_name),'fid')
+    for rootdir,dirs,files in os.walk(db_path):
+        for files_name in files:
+            make_db_data(os.path.join(rootdir,files_name),'fid')
+    for rootdir,dirs,files in os.walk(pic_path):
+        for files_name in files:
+            change_picname(os.path.join(rootdir,files_name),'fid')
+    tkMessageBox.showinfo(title='', message='照片更名完毕！')
+
 
 
 def main():
