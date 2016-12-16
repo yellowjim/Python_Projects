571e292d0f6d422722109e8d060aac4a6635ddc5
yellowjim <yellowjim@foxmail.com>
2016-12-17 02:52:56 +0800

    批量壮语书写

--- Zhuang_language.py
+++ Zhuang_language.py
@@ -3,36 +3,71 @@ import urllib2
 import urllib
 import mysql.connector
 import re
+import csv
+from Tkinter import *
 
-dic={'那','Naz'}
+db_place = {}
+dic_zhuangyu={u'那':'Naz'}
+p=re.compile('\\r\\n\\t\\t([a-z]+)\\t\\t')
 
-def choose_word(word):
-    temp_l=[]
+def choose_word(word,flag):
     temp_dic={}
     for x in word:
         len_word=len(x)
-        temp_l.append(len_word)
         temp_dic[len(x)]=x
-    temp_l.sort()
-    return temp_dic[temp_l[0]].capitalize()
+    temp_dic=sorted(temp_dic.iteritems(), key=lambda d: d[0], reverse=False)
+    print temp_dic
+    if flag==1:
+        for i,line in enumerate(temp_dic):
+            if i==1:
+                return temp_dic[i].capitalize()
+    else:
+        for i,line in enumerate(temp_dic):
+            if i==1:
+               return temp_dic[i]
 
-ip = raw_input('请输入IP地址：')
-db={}
-dmpc = mysql.connector.connect(user='root', password='GwGcgx@2016!',host=ip, database='dmpc1', use_unicode=True)
-cursor = dmpc.cursor()
-cursor.execute("SELECT f_id,f_name,f_nationalname from v_xingning where f_language='壮语' and f_nationalname=''")
-data=cursor.fetchall()
-for f_id,f_name,f_nationalname in data:
-    db[f_id]=(f_name,f_nationalname)
-cursor.close()
-dmpc.close()
-for x in db:
-    print db[x]
+def commit_ip(ipa):
+    ip=ipa
+    print ip
 
-p=re.compile('\\r\\n\\t\\t([a-z]+)\\t\\t')
-data = {'cih': '', 'js': '岭','t1':'提交'}
-post = urllib2.urlopen(
-        url= 'http://www.jiu60.com/hoiz/sawl.asp',
-        data= urllib.urlencode(data))
-l_word=p.findall(post.read())
-print choose_word(l_word)  #壮语译写首字母大写
+def query_word(word,flag):
+    if dic_zhuangyu.has_key(word) and flag==1:
+        return dic_zhuangyu[word].capitalize()
+    elif dic_zhuangyu.has_key(word) and flag!=1:
+        return dic_zhuangyu[word]
+    else:
+        js=word.encode('utf-8')
+        post_data = {'cih': '', 'js': js, 't1': '提交'}
+        post = urllib2.urlopen(
+                url= 'http://www.jiu60.com/hoiz/sawl.asp',
+                data= urllib.urlencode(post_data))
+        l_word=p.findall(post.read())
+        if word==[]:
+            return ''
+        elif flag==1:
+            return choose_word(l_word,1)
+        else:
+            return choose_word(l_word,0)
+
+def main():
+    ip=raw_input("输入IP地址：")
+    dmpc = mysql.connector.connect(user='root', password='GwGcgx@2016!',host=ip, database='dmpc1', use_unicode=True)
+    cursor = dmpc.cursor()
+    cursor.execute("SELECT f_id,f_name,f_nationalname from v_xingning where f_language='壮语' and f_name like '那%'")
+    data=cursor.fetchall()
+    for f_id,f_name,f_nationalname in data:
+        db_place[f_id]=(f_name,f_nationalname)
+    for place in db_place:
+        for sub_word in db_place[place][0]:
+            if db_place[place][0][0]==sub_word:
+                db_place[place]=(db_place[place][0],db_place[place][1]+query_word(sub_word,1))
+            else:
+                db_place[place] = (db_place[place][0], db_place[place][1] + query_word(sub_word, 0))
+        print db_place[place]
+    cursor.close()
+    dmpc.close()
+    for place in db_place:
+        print db_place[place][1]
+
+if __name__ == '__main__':
+    main()
